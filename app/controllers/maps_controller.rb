3e7720514ac4e3bc7ae887f18de77da7cd33e0f7
Dimitri Bosch <dimitribosch@hotmail.com>
2019-03-09 12:02:26 +0100

    Install mapbox

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,6 +1,6 @@
 class MapsController < ApplicationController
   skip_before_action :authenticate_user!
   def show
-    'hello'
+    @markers = [{ lng: 52.537693, lat: 13.4246498 }]
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,4 @@
 import "bootstrap";
+import { initMapbox } from '../plugins/init_mapbox';
+import 'mapbox-gl/dist/mapbox-gl.css';
+initMapbox();
