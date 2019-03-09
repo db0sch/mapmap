bb05d42838f8be700a3ae6c3431d066afebddf2f
Dimitri Bosch <dimitribosch@hotmail.com>
2019-03-09 18:23:54 +0100

    Fix filters

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,6 +1,8 @@
 class MapsController < ApplicationController
   skip_before_action :authenticate_user!
   def show
-    @markers = Place.all.map(&:coordinates)
+    @places = Place.all
+    @places = @places.tagged_with(params[:filters], any: true) if params[:filters].present?
+    @markers = @places.map(&:coordinates)
   end
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,11 @@
 class Place < ApplicationRecord
-  geocoded_by :address
+  geocoded_by :address do |obj,results|
+    if geo = results.first
+      obj.location_list.add(geo.city)
+      obj.latitude = geo.latitude
+      obj.longitude = geo.longitude
+    end
+  end
   after_validation :geocode, if: :will_save_change_to_address?
 
   acts_as_taggable_on :tags, :locations
